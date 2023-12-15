87001308b60852361416d06ec9bf4779bbb11c4f
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-15 15:20:51 +0000

    [start] #1 Variable_Chapter_sample.js Varによる変数の再代入

--- sample.js
+++ sample.js
@@ -1,20 +1,6 @@
-// varによる再宣言
+// varによる再代入
 var nickname = "taro";
 console.log(nickname);
 
-var nickname = "ichiro";
+nickname = "jiro";
 console.log(nickname);
-
-// letによる再宣言
-// let nickname = "taro";
-// console.log(nickname);
-
-// let nickname = "ichiro";
-// console.log(nickname);
-
-// constによる再宣言
-// const nickname = "taro";
-// console.log(nickname);
-
-// const nickname = "ichiro";
-// console.log(nickname);
